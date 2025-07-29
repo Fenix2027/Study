307538becd2f0a64c653c295cd367de14d7bdaed
Konstantsin <k4164724@gmail.com>
2025-07-29 21:47:31 +0300

    3

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6); // your code
-let dice2 = Math.floor(Math.random() * 6);// your code
-let isWinningDouble = (dice1===dice2)&&(dice1 > 3); // your code
+let dice1 = 1 + Math.floor(Math.random() * 5); // your code
+let dice2 = 1 + Math.floor(Math.random() * 5);// your code
+let isWinningDouble = (dice1 === dice2)&&(dice1 > 3); // your code
 
 
 console.log('Первый бросок: ' + dice1)
