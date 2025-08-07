b90d30c631ccb0e8bcaf2330fa47bc2811cdf9ee
Konstantsin <k4164724@gmail.com>
2025-08-07 21:52:01 +0300

    13

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -15,11 +15,11 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 
 function includesElement(array,element) {
     for (i = 0; i < array.length; i++) {
-        if (element === array[i]) {
-            return true
-        } else {
-            return false
-        }
-        
-    }
-}
+        if (array[i] === element) {
+            return true;
+        }       
+    } 
+     return false;
+};
+ 
+console.log(includesElement([1,2,3],2));
