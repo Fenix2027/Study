3d6db5d5eac3eb397aa57d1773eeec3bd5d0f99f
Konstantsin <k4164724@gmail.com>
2025-08-29 01:24:15 +0300

    3

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,12 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(object, name , update) {
+      
+   return  object[name] = update ;
+ 
+}
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+console.log(gallery);
+
