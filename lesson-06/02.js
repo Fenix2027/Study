1d2ec80bec34eab002cd82e0d0c6351adfd18283
Konstantsin <k4164724@gmail.com>
2025-09-07 23:09:06 +0300

    3

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,22 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+function map(array,callback)  {
+  const variable = [];
+  for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+
+       variable.push(callback(element,index))
+      
+  }
+      
+return variable;
+  
+}
+const numbers = [1, 2, 3, 4, 5]
+
+const doubledNumbers = map(numbers, (element, index) => {
+  return element * 2
+})
+
+console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
